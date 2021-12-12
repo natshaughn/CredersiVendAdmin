02cfb4fa1df34fb0e60b11130c2403a3915a30ed
Laurence Martin <floz@credersi.com>
2021-12-12 13:45:39 +0000

    Enhanced to support url parameters for direct access

--- backend/src/main/java/com/credersi_vend/backend/jwt/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/credersi_vend/backend/jwt/JwtAuthenticationEntryPoint.java
@@ -15,7 +15,39 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 	private static final long serialVersionUID = 1L;
 	
 	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {		
-		response.sendRedirect("/login");
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {	
+		String domain = request.getParameter("domain");
+		String customer = request.getParameter("customer");
+		String site = request.getParameter("site");
+		String machines = request.getParameter("machines");
+		
+		boolean noDomain = ((domain == null) || (domain.length() < 1));
+		boolean noCustomer = ((customer == null) || (customer.length() < 1));
+		boolean noSite = ((site == null) || (site.length() < 1));
+		boolean noMachines = ((machines == null) || (machines.length() < 1));
+		
+		if (noDomain && noCustomer && noSite && noMachines) {
+			response.sendRedirect("/login");	
+		} else {
+			String params = "";
+			
+			if (!noDomain) {
+				params = "domain="+domain;
+				
+				if (!noCustomer) {
+					params += "&customer="+customer;
+					
+					if (!noSite) {
+						params += "&site="+site;
+						
+						if (!noMachines) {
+							params += "&machines="+machines;	
+						}
+					}
+				}
+			}
+			
+			response.sendRedirect("/login?"+params);			
+		}
 	}
 }
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -16,12 +16,87 @@
 	$: hasMachines = (context && context.machines && (context.machines.length > 0));
 
 	const refresh = (event) => {
-		context = event.detail;
-	};
+		const url = new URL(window.location.href);
 
+		if (event.root !== true) {
+			context = event.detail;
+		} else {
+			const domain = event.detail.domain;
+			const domainUuid = event.detail.domain.uuid;
+			const domainUrl = `/api/v1/domains/${domainUuid}`;
+			const customerUuid = url.searchParams.get('customer');
+			const siteUuid = url.searchParams.get('site');
+			const machineUuids = url.searchParams.get('machines');
+
+			if ((customerUuid != null) && (customerUuid.length > 0)) {
+				const customerUrl = domainUrl+`/customers/${customerUuid}`;
+				fetch(customerUrl)
+					.then((response) => response.json())
+					.then((customer) => {
+						if ((siteUuid != null) && (siteUuid.length > 0)) {
+							const siteUrl = customerUrl+`/sites/${siteUuid}`;
+							fetch(siteUrl)
+								.then((response) => response.json())
+								.then(async (site) => {
+									if ((machineUuids != null) && (machineUuids.length > 0)) {
+										const machines = [];
+										const split = machineUuids.split(' ');
+										
+										for (let machineUuid of split) {
+											const machineResult = await fetch(siteUrl+`/machines/${machineUuid}`);
+											const machine = await machineResult.json();
+											machines.push(machine);
+										}
+
+										context = {domain, customer, site, machines};
+									} else {
+										context = {domain, customer, site};
+									}
+								}).catch((err) => alert('Failed to fetch context details!'));
+						} else {
+							context = {domain, customer};
+						}
+					}).catch((err) => alert('Failed to fetch context details!'));
+			} else {
+				context = event.detail;
+			}
+			
+			return;
+		}
+
+		if (context.domain && context.domain.uuid) {
+			url.searchParams.set('domain', context.domain.uuid);
+		}
+
+		if (context.customer && context.customer.uuid) {
+			url.searchParams.set('customer', context.customer.uuid);
+		} else {
+			url.searchParams.delete('customer');
+		}
+
+		if (context.site && context.site.uuid) {
+			url.searchParams.set('site', context.site.uuid);
+		} else {
+			url.searchParams.delete('site');
+		}
+
+		if (context.machines && (context.machines.length > 0)) {
+			let values = null;
+			for (let machine of context.machines) {
+				values = (values) ? values+'+'+machine.uuid : machine.uuid;
+			}
+
+			url.searchParams.set('machines', values);
+		} else {
+			url.searchParams.delete('machines');
+		}
+				
+		window.history.pushState({path: url.href}, '', url.href);
+	};
+	
 	fetch('/api/v1/domains')
 		.then((response) => response.json())
-		.then((domain) => refresh({detail: {domain}}))
+		.then((domain) => refresh({detail: {domain}, root: true}))
 		.catch((err) => alert('Failed to fetch domains!'));
 </script>
 
